c855c46116e270e055497b9e27294b72d5dd7c1a
Sachin Ahuja <sachinahj@gmail.com>
2014-07-01 20:45:40 -0500

    new laptop initial commit

--- src/myquery.js
+++ src/myquery.js
@@ -6,8 +6,28 @@
 
   var myQuery = function (selector) {
     // TODO
+    identifier = selector.slice(0,1)
+    if (identifier === '.') {
+      elems = document.getElementbyClass(selector.slice(1));
+    } else if (identifier === '#') {
+      elems = document.getElementbyID(selector.slice(1));
+    } else {
+      elems = document.getElementbyTagName(selector.slice(1));
+    }
+
+    return new QueryWrapper(elems); 
   };
 
+  
+  myQuery.each = function (array, func) {
+    for (var i = 0; i < array.length; i++) {
+      func(array[i]);
+    }
+  };
+
+
+
+  myQuery.version = 'beta';
   window.$ = myQuery;
 
 })();
